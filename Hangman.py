25056ade302174d82e00cc68ba94b9f52773de00
jhbb9 <40704743+jhbb9@users.noreply.github.com>
2023-11-22 16:49:45 -0600

    Create Hangman.py

---
+++ Hangman.py
@@ -0,0 +1,148 @@
+HANGMAN = [
+    """
+ ------
+|     |
+|
+|
+|
+|
+|
+|
+----------
+""",
+    """
+ ------
+|     |
+|     0
+|
+|
+|
+|
+|
+----------
+""",
+    """
+ ------
+|     |
+|     0
+|     +
+|
+|
+|
+|
+----------
+""",
+    """
+ ------
+|     |
+|     0
+|    -+
+|
+|
+|
+|
+----------
+""",
+    """
+ ------
+|     |
+|     0
+|    -+-
+|
+|
+|
+|
+----------
+""",
+    """
+ ------
+|     |
+|     0
+|   /-+-
+|
+|
+|
+|
+----------
+""",
+    """
+ ------
+|     |
+|     0
+|   /-+-/
+|
+|
+|
+|
+----------
+""",
+    """
+ ------
+|     |
+|     0
+|   /-+-/
+|     |
+|
+|
+|
+----------
+""",
+    """
+ ------
+|     |
+|     0
+|   /-+-/
+|     |
+|     |
+|
+|
+----------
+""",
+    """
+ ------
+|     |
+|     0
+|   /-+-/
+|     |
+|     |
+|    |
+|    |
+----------
+""",
+    """
+ ------
+|     |
+|     0
+|   /-+-/
+|     |
+|     |
+|    | |
+|    | |
+----------
+"""
+]
+
+secretWork = list("buttholelicker")
+
+guessingWord = list(("_"*len(secretWork)))
+
+overall_wrong_count = 0
+loop_wrong_count = 0
+
+while overall_wrong_count != 10 or secretWork != guessingWord:
+    print(HANGMAN[overall_wrong_count])
+    print(guessingWord)
+    inputed_str = input("input your guess please: ")
+    loop_wrong_count=0
+    for i in range(len(secretWork)):
+        if inputed_str == secretWork[i]:
+            print(inputed_str)
+            guessingWord[i] = secretWork[i]
+            loop_wrong_count += 1
+
+    if loop_wrong_count == 0:
+        overall_wrong_count += 1
+    if overall_wrong_count >= 11:
+        print('You lost Cunt!')
+    elif guessingWord == secretWork:
+        print('You win Kunt!')
